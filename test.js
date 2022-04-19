d91c96327a84b566106d0315fb34a2079d9f429a
Veronika Luhova <nikanika1909@gmail.com>
2022-04-20 00:26:08 +0300

    +

--- test.js
+++ test.js
@@ -43,5 +43,24 @@ let item_5 = item_3;
 // 19. Присвоить переменной item_6 значение 15
 // 20. Присвоить переменной item_6_type тип переменной item_6
 
+let item_6 = 15;
+let item_6_type = typeof(item_6);
+
+// 21. Вывести в консоль тип данных item_6 в виде ——  “item_6 == ”  item_6,  “item_6_type == ”  item_6_type ——  
+
+console.log(item_6 == item_6);
+console.log(item_6_type == item_6);
+console.log(item_6_type);
+
+// 22. Создать переменную item_7 и в ней преобразовать item_6 в String.
+
+let item_7 = String(item_6);
+
+// 23. Создать переменную item_7_type
+// 24. Присвоить переменной item_7_type тип переменной item_7
+
+let item_7_type = typeof(item_7);
+
+console.log(item_7_type);
 
 
