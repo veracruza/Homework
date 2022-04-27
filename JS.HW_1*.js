03489a2b49a7e0f55e0864fa9a5321c29827541b
Veronika Luhova <nikanika1909@gmail.com>
2022-04-27 15:51:20 +0300

    +2*

--- JS.HW_1*.js
+++ JS.HW_1*.js
@@ -42,17 +42,19 @@ const checkAge = function (age){
         if (age < age_2) {
              
               console.log("You don’t have access cause your age is " + age + " It’s less then");
-         }
-            else if (age >=  age_2 && age <  age_3){
+         } else if (age >=  age_2 && age <  age_3){
                 
                 console.log("Welcome !");
-                }
-            else if (age  >= age_3 ){
+         } else if (age  >= age_3 ){
                 
                 console.log("Keep calm and look Culture channel");
-            }
-        } 
-    else {
-            console.log("Technical work");     
-      }  
+         } else {
+            console.log("Technical work"); 
+         } 
+    } else { console.log("Not integer value");}
+            
 }
+
+checkAge(17);
+checkAge("fere");
+
