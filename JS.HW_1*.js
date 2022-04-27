abb93804c1c83048fd4a19ee37e757f38bc21b85
Veronika Luhova <nikanika1909@gmail.com>
2022-04-27 15:41:16 +0300

    +1*

--- JS.HW_1*.js
+++ JS.HW_1*.js
@@ -9,6 +9,33 @@ let age_1 = 10;
 let age_2 = 18;
 let age_3 = 60;
 
+// const checkAge = function (age){
+
+//         if (age < age_2) {
+             
+//               console.log("You don’t have access cause your age is " + age + " It’s less then");
+//          }
+//             else if (age >=  age_2 && age <  age_3){
+                
+//                 console.log("Welcome !");
+//                 }
+//             else if (age  >= age_3 ){
+                
+//                 console.log("Keep calm and look Culture channel");
+//             }
+//         else {
+//                 console.log("Technical work");     
+//         }  
+// }
+
+// checkAge(17);
+// checkAge(19);
+// checkAge("fere");
+
+// 2*:
+// Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
+
+
 const checkAge = function (age){
     if (typeof(age) == "number"){
 
@@ -24,11 +51,8 @@ const checkAge = function (age){
                 
                 console.log("Keep calm and look Culture channel");
             }
-       } 
+        } 
     else {
             console.log("Technical work");     
       }  
 }
-
-checkAge(17);
-checkAge(19);
