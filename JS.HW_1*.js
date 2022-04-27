8224d2a2287a002cf7019d75c07868f7bd8af0f2
Veronika Luhova <nikanika1909@gmail.com>
2022-04-27 17:17:23 +0300

    +3*

--- JS.HW_1*.js
+++ JS.HW_1*.js
@@ -36,8 +36,41 @@ let age_3 = 60;
 // Преобразовать задание 1* таким образом, чтобы первым делом в функции проверялся тип данных. И если он не Number - кидалась ошибка.
 
 
+// const checkAge = function (age){
+//     if (typeof(age) == "number"){
+
+//         if (age < age_2) {
+             
+//               console.log("You don’t have access cause your age is " + age + " It’s less then");
+//          } else if (age >=  age_2 && age <  age_3){
+                
+//                 console.log("Welcome !");
+//          } else if (age  >= age_3 ){
+                
+//                 console.log("Keep calm and look Culture channel");
+//          } else {
+//             console.log("Technical work"); 
+//          } 
+//     } else { console.log("Not integer value");}
+            
+// }
+
+// checkAge(17);
+// checkAge("fere");
+
+// 3**:
+// Преобразовать 2* таким образом, чтобы значение '2' (строка в которой лежит ТОЛЬКО ЦИФРА) пропускалось, преобразовываясь в number
+
+
+
+
 const checkAge = function (age){
-    if (typeof(age) == "number"){
+
+    // age = Number(age)
+    // if (!isNaN(age)) {
+    // if (Number(age)) {
+   
+    if (+age) {
 
         if (age < age_2) {
              
@@ -55,6 +88,5 @@ const checkAge = function (age){
             
 }
 
-checkAge(17);
-checkAge("fere");
-
+checkAge ("22")
+checkAge ("qweer")
